3d2c6caa13f501c17e966bbf0d36443077637f5c
myrtokonst1 <ucabm@ucl.ac.uk>
2022-08-18 09:50:08 +0100

    prs start

--- kernels/safe_rbf_kernel.py
+++ kernels/safe_rbf_kernel.py
@@ -1,7 +1,10 @@
 import random
 
+import gpflow
 from gpflow.kernels import RBF
 import tensorflow as tf
+from gpflow.utilities.ops import square_distance
+
 
 def generate_random_small_number():
     exp = random.randint(-7, -5)
@@ -10,7 +13,14 @@ def generate_random_small_number():
 
 
 class SafeRBF(RBF):
+    def __init__(self):
+        super().__init__(self.variance, self.lengthscales)
+
     def scaled_squared_euclid_dist(self, X, X2=None) -> tf.Tensor:
         random_small_number = generate_random_small_number()
-        r2 = self.square_dist(self.scale(X), self.scale(X2))
+        print(random_small_number)
+
+        r2 = square_distance(self.scale(X), self.scale(X2))
+        print(r2)
+        print(r2+ random_small_number)
         return tf.sqrt(r2 + random_small_number) # or 1e-6. I find 1e-12 is too small
